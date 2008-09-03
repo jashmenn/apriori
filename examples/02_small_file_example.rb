d18fd6b5f302f8fae123b552799cc8245e75122e
Nate Murray <nate@natemurray.com>
2008-09-03 07:54:18 -0700

    fixed small example file

--- examples/02_small_file_example.rb
+++ examples/02_small_file_example.rb
@@ -2,21 +2,16 @@ $:.unshift(File.dirname(__FILE__) + "/../lib")
 require 'apriori'
 require 'benchmark'
 
-# NOTE: the 'accidents.dat' file is not supplied with this gem. To download it, 
-# run: rake apriori:get_example_data 
-# or see http://fimi.cs.helsinki.fi/data/
-
-puts "This will take a little while. Hold tight..."
-
 rules = nil
 results = Benchmark.measure {
-rules = Apriori.find_association_rules(File.dirname(__FILE__) + "/test_data/accidents.dat",
+rules = Apriori.find_association_rules(File.dirname(__FILE__) + "/test_data/market_basket_basic_test.dat",
                            :min_items => 2,
-                           :max_items => 10,
-                           :min_support => 50, 
-                           :min_confidence => 80)
+                           :max_items => 3,
+                           :min_support => 0.01, 
+                           :min_confidence => 30)
 }
 
+puts rules.join("\n")
 puts "#{rules.size} rules generated"
 puts "Benchmarks:"
 puts results
